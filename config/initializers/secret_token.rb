ec97422431957599a15bc227ed0a266dbec5e259
Eric Chang <changey@mit.edu>
2013-11-14 20:19:00 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amis::Application.config.secret_token = '77f60ab8b33298d7a2f111419d52b3fe027983ac4364b2d8ef5b5e63cd79599d5085f4bc5810d30015349448f95776dfe7c2fab6d73c47d18224847b1dc35df3'
